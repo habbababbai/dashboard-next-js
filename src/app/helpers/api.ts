fc5c578ba5abca4d30c6883b7414e416598d6198
habbababbai <qba.7145@gmail.com>
2025-07-23 22:19:46 +0200

    BUGFIX: fix user email endpoint

--- src/app/helpers/api.ts
+++ src/app/helpers/api.ts
@@ -1,14 +1,9 @@
 import { User } from "@/app/types/user";
 
 export async function getUserByEmail(email: string): Promise<User | null> {
-    const baseUrl =
-        process.env.NEXTAUTH_URL ||
-        process.env.NEXT_PUBLIC_BASE_URL ||
-        "http://localhost:3000";
-    const res = await fetch(
-        `${baseUrl}/api/user?email=${encodeURIComponent(email)}`,
-        { cache: "no-store" }
-    );
+    const res = await fetch(`/api/user?email=${encodeURIComponent(email)}`, {
+        cache: "no-store",
+    });
     if (!res.ok) return null;
     return res.json();
 }
